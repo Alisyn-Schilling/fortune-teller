5b8db1f004c1afa92668a7236bfeaecdc6d2f7de
Alisyn S <aeschilling.87@gmail.com>
2020-05-12 12:01:32 -0400

    finished fortune teller

--- src/org/wecancodeit/FortuneTellerApp.java
+++ src/org/wecancodeit/FortuneTellerApp.java
@@ -19,7 +19,7 @@ public class FortuneTellerApp {
         Scanner input = new Scanner(System.in);
 
         System.out.println("What is your first name:");
-        String firstName= input.nextLine();
+        String firstName = input.nextLine();
         System.out.println("Your first name is " + firstName);
 
         System.out.println("What is your last name:");
@@ -37,9 +37,9 @@ public class FortuneTellerApp {
 
         System.out.println("What is your favorite ROYGBIV color, type HELP if you don't know the colors:");
         String favoriteColor = input.nextLine();
-        while (favoriteColor.equals("HELP")){
-            System.out.println("Red Orange Yellow Green Blue Indigo and Violet");
-            System.out.println("What is your favorite ROYGBIV color, type HELP if you don't know the colors:");
+        //while (favoriteColor.equals("HELP")) {
+        if (favoriteColor.equalsIgnoreCase("help")) {
+            System.out.println("ROYGBIV stands for Red Orange Yellow Green Blue Indigo and Violet");
             favoriteColor = input.nextLine();
         }
         System.out.println("Your favorite color is " + favoriteColor);
@@ -47,5 +47,75 @@ public class FortuneTellerApp {
         System.out.println("How many siblings do you have:");
         int siblingCount = input.nextInt();
         System.out.println("You have " + siblingCount + " siblings.");
+
+        //The user's number of years until retirement will be based on whether the user's age is odd or even.
+        int yearsToRetirement;
+        if (userAge % 2 == 0) {
+            yearsToRetirement = 12;
+        } else {
+            yearsToRetirement = 14;
+        }
+
+        //System.out.println("Years To Retirement: " + yearsToRetirement);
+
+        //The location of the user's vacation home will be based on how many siblings the user has.
+        // If the user enters a number less than zero, give the user a bad location!
+        String vacationHomeLocation = "Chernobyl, Ukraine";
+        if (siblingCount == 0) {
+            vacationHomeLocation = "Boca Raton, FL";
+        } else if (siblingCount == 1) {
+            vacationHomeLocation = "Nassau, Bahamas";
+        } else if (siblingCount == 2) {
+            vacationHomeLocation = "Ponta Negra, Brazil";
+        } else if (siblingCount == 3) {
+            vacationHomeLocation = "Portland, Oregon";
+        } else if (siblingCount > 3) {
+            vacationHomeLocation = "Baton Rouge, LA";
+        }
+        //System.out.println("Your Vacation Home is in " + vacationHomeLocation);
+
+        String modeOfTransportation;
+        switch (favoriteColor.toLowerCase()) {
+            case "red":
+                modeOfTransportation = "Maserati";
+                break;
+            case "orange":
+                modeOfTransportation = "seven chihuahuas pulling a chariot.";
+                break;
+            case "yellow":
+                modeOfTransportation = " school bus";
+                break;
+            case "green":
+                modeOfTransportation = "Tesla";
+                break;
+            case "blue":
+                modeOfTransportation = "Yugo";
+                break;
+            case "indigo":
+                modeOfTransportation = "Honda Civic";
+                break;
+            case "violet":
+                modeOfTransportation = "Mini Cooper";
+                break;
+            default:
+                modeOfTransportation = "taxi cab";
+
+        }
+        //System.out.println("Your mode of transportation will be a " + modeOfTransportation);
+
+        double bankBalance = 0.0;
+        if (userBirthMonth > 0 && userBirthMonth <= 4){
+            bankBalance = 256000.76;
+        } else if (userBirthMonth > 4&& userBirthMonth <=8 ){
+            bankBalance = 3687105.42;
+        } else if (userBirthMonth > 8 && userBirthMonth <=12){
+            bankBalance = 45.65;
+        }
+        //System.out.println("You have $" + bankBalance + " in the bank.");
+
+        System.out.println(firstName+ " " + lastName + " will retire in " + yearsToRetirement + "\n" +
+                "with " + bankBalance + " in the bank,\n" +
+                "a vacation home in " + vacationHomeLocation + ",\n" +
+                "and travel by " + modeOfTransportation + ".");
     }
 }
