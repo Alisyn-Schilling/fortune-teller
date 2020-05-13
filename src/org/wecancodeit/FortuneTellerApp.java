701fc342a8a423652c0391f9544da42781511e5e
Alisyn S <aeschilling.87@gmail.com>
2020-05-13 13:55:50 -0400

    Refactored into methods

--- src/org/wecancodeit/FortuneTellerApp.java
+++ src/org/wecancodeit/FortuneTellerApp.java
@@ -4,61 +4,89 @@ import java.util.Scanner;
 
 public class FortuneTellerApp {
 
-    /*
-     * Part 1
-    Ask the user for the user’s first name.
-    Ask the user for the user’s last name.
-    Ask the user for the user’s age.
-    Ask the user for the user’s birth month (as an 'int').
-    Ask the user for the user’s favorite ROYGBIV color.
-        If the user does not know what ROYGBIV is, ask the user to enter “Help” to get a list of the ROYGBIV colors.
-    Ask the user for the user's number of siblings.
-     */
     public static void main(String[] args) {
         System.out.println("Hello, welcome to  We Can Code IT's Fortune Teller Application!");
         Scanner input = new Scanner(System.in);
-
-        System.out.println("What is your first name:");
-        String firstName = input.nextLine();
+        String firstName = getFirstName(input);
         System.out.println("Your first name is " + firstName);
 
-        System.out.println("What is your last name:");
-        String lastName = input.nextLine();
+        String lastName = getLastName(input);
         System.out.println("Your last name is " + lastName);
 
-        System.out.println("What is your age:");
-        int userAge = input.nextInt();
+        int userAge = getAge(input);
         System.out.println("Your age is " + userAge);
 
-        System.out.println("What is your birth month:");
-        int userBirthMonth = input.nextInt();
-        input.nextLine();
+        int userBirthMonth = getBirthMonth(input);
         System.out.println("You birth month is " + userBirthMonth);
+        input.nextLine();
 
+        String favoriteColor = getFavoriteColor(input);
+        System.out.println("Your favorite color is " + favoriteColor);
+
+        int siblingCount = getSiblingCount(input);
+        System.out.println("You have " + siblingCount + " siblings.");
+
+        int yearsToRetirement = getRetirement(userAge);
+        String vacationHomeLocation = getVacationHome(siblingCount);
+        String modeOfTransportation = getModeOfTransportation(favoriteColor);
+        double bankBalance = getBankBalance(userBirthMonth);
+        System.out.println(firstName + " " + lastName + " will retire in " + yearsToRetirement + " years,\n" +
+                "with " + bankBalance + " in the bank,\n" +
+                "a vacation home in " + vacationHomeLocation + ",\n" +
+                "and travel by " + modeOfTransportation + ".");
+    }
+
+    public static String getFirstName(Scanner input) {
+        System.out.println("What is your first name:");
+        return input.nextLine();
+    }
+
+    public static String getLastName(Scanner input) {
+        System.out.println("What is your last name:");
+        return input.nextLine();
+    }
+
+    public static int getAge(Scanner input) {
+        System.out.println("What is your age:");
+        return input.nextInt();
+    }
+
+    public static int getBirthMonth(Scanner input) {
+        System.out.println("What is your birth month:");
+        int birthMonth = input.nextInt();
+        while ((birthMonth < 1) || (12 < birthMonth)){
+            System.out.println("Birth month number bust be between 1 and 12");
+            birthMonth = input.nextInt();
+        }
+        return birthMonth;
+    }
+
+    public static String getFavoriteColor(Scanner input) {
         System.out.println("What is your favorite ROYGBIV color, type HELP if you don't know the colors:");
         String favoriteColor = input.nextLine();
         while (favoriteColor.equalsIgnoreCase("help")) {
             System.out.println("ROYGBIV stands for Red Orange Yellow Green Blue Indigo and Violet");
             favoriteColor = input.nextLine();
         }
-        System.out.println("Your favorite color is " + favoriteColor);
+        return favoriteColor;
+    }
 
+    public static int getSiblingCount(Scanner input) {
         System.out.println("How many siblings do you have:");
-        int siblingCount = input.nextInt();
-        System.out.println("You have " + siblingCount + " siblings.");
+        return input.nextInt();
+    }
 
-        //The user's number of years until retirement will be based on whether the user's age is odd or even.
+    public static int getRetirement(int userAge) {
         int yearsToRetirement;
         if (userAge % 2 == 0) {
             yearsToRetirement = 12;
         } else {
             yearsToRetirement = 14;
         }
+        return yearsToRetirement;
+    }
 
-        //System.out.println("Years To Retirement: " + yearsToRetirement);
-
-        //The location of the user's vacation home will be based on how many siblings the user has.
-        // If the user enters a number less than zero, give the user a bad location!
+    public static String getVacationHome(int siblingCount) {
         String vacationHomeLocation = "Chernobyl, Ukraine";
         if (siblingCount == 0) {
             vacationHomeLocation = "Boca Raton, FL";
@@ -71,8 +99,10 @@ public class FortuneTellerApp {
         } else if (siblingCount > 3) {
             vacationHomeLocation = "Baton Rouge, LA";
         }
-        //System.out.println("Your Vacation Home is in " + vacationHomeLocation);
+        return vacationHomeLocation;
+    }
 
+    public static String getModeOfTransportation(String favoriteColor) {
         String modeOfTransportation;
         switch (favoriteColor.toLowerCase()) {
             case "red":
@@ -98,23 +128,19 @@ public class FortuneTellerApp {
                 break;
             default:
                 modeOfTransportation = "taxi cab";
-
         }
-        //System.out.println("Your mode of transportation will be a " + modeOfTransportation);
+        return modeOfTransportation;
+    }
 
+    public static double getBankBalance(int userBirthMonth) {
         double bankBalance = 0.0;
-        if (userBirthMonth > 0 && userBirthMonth <= 4){
+        if (userBirthMonth > 0 && userBirthMonth <= 4) {
             bankBalance = 256000.76;
-        } else if (userBirthMonth > 4&& userBirthMonth <=8 ){
+        } else if (userBirthMonth > 4 && userBirthMonth <= 8) {
             bankBalance = 3687105.42;
-        } else if (userBirthMonth > 8 && userBirthMonth <=12){
+        } else if (userBirthMonth > 8 && userBirthMonth <= 12) {
             bankBalance = 45.65;
         }
-        //System.out.println("You have $" + bankBalance + " in the bank.");
-
-        System.out.println(firstName+ " " + lastName + " will retire in " + yearsToRetirement + "\n" +
-                "with " + bankBalance + " in the bank,\n" +
-                "a vacation home in " + vacationHomeLocation + ",\n" +
-                "and travel by " + modeOfTransportation + ".");
+        return bankBalance;
     }
 }
